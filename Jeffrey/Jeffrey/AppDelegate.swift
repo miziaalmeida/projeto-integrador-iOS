c2562eb715b56366ac397383b656fbc22eb7ab3c
Mízia Lima <mizia.alima@gmail.com>
2020-12-03 20:57:46 -0300

    Reajusta projeto na develop para criacao de novas features utilizando arquitetura MVVM

--- Jeffrey/Jeffrey/AppDelegate.swift
+++ Jeffrey/Jeffrey/AppDelegate.swift
@@ -1,18 +1,10 @@
 import UIKit
-import Firebase
-import FBSDKCoreKit
-import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-     
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-//        FirebaseApp.configure()
-//        GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
-        
+    
         return true
     }
 
@@ -29,9 +21,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return (GIDSignIn.sharedInstance()?.handle(url))!
-    }
-    
 }
--- Jeffrey/Jeffrey/Model/Login/User.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import UIKit
-
-struct User {
-    let name: String
-    let email: String
-    let password: String
-    let image: UIImage!
-}
