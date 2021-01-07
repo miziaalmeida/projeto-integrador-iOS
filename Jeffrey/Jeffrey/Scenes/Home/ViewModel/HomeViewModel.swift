b8177fe732209f3494c2b324937486f624231409
Michel dos Santos <michelsantos15@gmail.com>
2021-01-07 01:38:42 -0300

    Começo

--- Jeffrey/Jeffrey/Scenes/Home/ViewModel/HomeViewModel.swift
+++ Jeffrey/Jeffrey/Scenes/Home/ViewModel/HomeViewModel.swift
@@ -7,4 +7,34 @@
 
 import Foundation
 
+protocol HomeViewModelProtocol: AnyObject{
+    
+    func raffleListOfAPIMovies(textSearch: String ,completion: @escaping (Bool) -> Void)
+   
+
+}
+
+class HomeViewModel:HomeViewModelProtocol{
+    
+    let apimanager = SelectedMovieAPI()
+    
+    var arrayMoviesComedy = [Movie]()
+    
+    func raffleListOfAPIMovies(textSearch: String ,completion: @escaping (Bool) -> Void) {
+        
+        apimanager.listOfFilmswithoutProvider(idPage: 1, genre: 16) { (arrayMovieComedy) in
+            
+            for movie in arrayMovieComedy{
+                arrayMoviesComedy.append(Movie(fromDictionary: movie))
+            }
+            completion(true)
+            return
+        }
+        
+        
+    
+    
+}
+
+}
 
