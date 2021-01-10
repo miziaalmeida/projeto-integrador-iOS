1679dd9a6825dfa790db3ea37f534c76a63f398e
Michel dos Santos <michelsantos15@gmail.com>
2021-01-10 00:59:15 -0300

    ajusta botões dos filtros provedores e generos

--- Jeffrey/Jeffrey/Scenes/Tip/View/TipViewController.swift
+++ Jeffrey/Jeffrey/Scenes/Tip/View/TipViewController.swift
@@ -21,6 +21,10 @@ class TipViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonStreaming.forEach{
+            $0.isSelected = false
+            $0.alpha = 0.3
+        }
         
 //        let customTabBarItem = UITabBarItem(title: "Dica", image: UIImage(named: "dica.png"),tag: 1)
 //        self.tabBarItem = customTabBarItem
@@ -101,9 +105,10 @@ class TipViewController: UIViewController {
     @IBAction func actionButtonGenre(_ sender: UIButton) {
         buttonGenre.forEach{
             $0.isSelected = false
+            $0.backgroundColor = #colorLiteral(red: 0.1137254902, green: 0.07450980392, blue: 0.2196078431, alpha: 1)
         }
         sender.isSelected = true
-        //sender.backgroundColor = UIColor.lightGray
+        sender.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
         if let unwarappedGenre = sender.currentTitle{
             self.gener = unwarappedGenre
             print(gener)
@@ -118,8 +123,10 @@ class TipViewController: UIViewController {
     @IBAction func actionButtonStreaming(_ sender: UIButton) {
         buttonStreaming.forEach{
             $0.isSelected = false
+            $0.alpha = 0.3
         }
         sender.isSelected = true
+        sender.alpha = 1
         if let unwrappedStream = sender.currentTitle {
             self.stream = unwrappedStream
     }
