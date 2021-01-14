85dc0357de21a09e5ca63096170272bc28981117
Taize Carminatti <taizecarminatti@gmail.com>
2021-01-14 09:39:34 -0300

    merge alteracoes

--- Jeffrey/Jeffrey/Scenes/Home/View/HomeViewController.swift
+++ Jeffrey/Jeffrey/Scenes/Home/View/HomeViewController.swift
@@ -21,11 +21,9 @@ class HomeViewController:  UIViewController, UITableViewDelegate, UITableViewDat
     @IBOutlet weak var tableViewHome:UITableView!
     
     var viewModel: HomeViewModelProtocol!
-<<<<<<< HEAD
+
     var activity = ActivityIndicatorViewController()
-=======
     
->>>>>>> develop
     
 
     // MARK: - LifeCircle
@@ -33,20 +31,22 @@ class HomeViewController:  UIViewController, UITableViewDelegate, UITableViewDat
         super.viewDidLoad()
         viewModel = HomeViewModel()
         
-        viewModel.raffleListOfAPIMovies(genre: 35) { (sucess) in
-            self.viewModel.raffleListOfAPIMovies(genre: 28) { (sucess) in
-                self.viewModel.raffleListOfAPIMovies(genre: 16) { (sucess) in
-                    self.viewModel.raffleListOfAPIMovies(genre: 27) { (sucess) in
-                        self.tableViewHome.delegate = self
-                        self.tableViewHome.dataSource = self
-                        self.tableViewHome.reloadData()
+        activity.showActivityIndicator(view: view, targetVC: self)
+                viewModel.raffleListOfAPIMovies(genre: 35) { (sucess) in
+                    self.viewModel.raffleListOfAPIMovies(genre: 28) { (sucess) in
+                        self.viewModel.raffleListOfAPIMovies(genre: 16) { (sucess) in
+                            self.viewModel.raffleListOfAPIMovies(genre: 27) { (sucess) in
+                                self.activity.hideActivityIndicator(view: self.view)
+                                self.tableViewHome.delegate = self
+                                self.tableViewHome.dataSource = self
+                                self.tableViewHome.reloadData()
+                            }
+                            
+                        }
+                       
                     }
                     
                 }
-               
-            }
-            
-        }
         tableViewHome.estimatedRowHeight = 85.0
         tableViewHome.rowHeight = UITableView.automaticDimension
         
@@ -72,7 +72,18 @@ class HomeViewController:  UIViewController, UITableViewDelegate, UITableViewDat
         
     }
     
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+            let tableHeader = view as! UITableViewHeaderFooterView
+              tableHeader.tintColor = UIColor.clear
+              tableHeader.textLabel?.textColor = .white
+              tableHeader.textLabel?.font = UIFont(name: "Helvetica-Neue", size: 25)
+              tableHeader.textLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        }
     
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            
+            return 40
+        }
     
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
